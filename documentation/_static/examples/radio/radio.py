0a4fde871728725478f11cb9656116e77ee1b99d
lijutsang <957790617@qq.com>
2025-06-26 12:30:41 +0800

    bug

--- documentation/_static/examples/radio/radio.py
+++ documentation/_static/examples/radio/radio.py
@@ -5,13 +5,12 @@ import random
 from mpython import *
 from radio import *
 
-#MPythonESPNow(wifi_ch=0)  wifi_ch:信道 0-14 整型
-espnow_0 = MPythonESPNow(wifi_ch=0) 
+#MPythonESPNow(wifi_ch=0)  wifi_ch:信道 0-11 整型
+espnow_0 = MPythonESPNow(wifi_ch=11) 
 
 #MPythonESPNow.get_mac(mode=0) mode:0 sta地址/1 ap地址 16进制字符串mac地址
-oled.fill(0)
-oled.DispChar(str(espnow_0.get_mac(mode=0)), 0, 0, 1)
-oled.show()
+print(str(espnow_0.get_mac(mode=0)), 0, 0, 1)
+
 
 print(espnow_0.get_mac(mode=0)) # sta 
 print(espnow_0.get_mac(mode=1)) # ap
