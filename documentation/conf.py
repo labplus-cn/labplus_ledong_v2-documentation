1024d7ee5fdb94383e605baa46152fea98f0c987
zengliju <957790617@qq.com>
2025-12-24 15:39:03 +0800

    epub

--- documentation/conf.py
+++ documentation/conf.py
@@ -70,7 +70,7 @@ html_static_path = ['_static']
 
 # 1. 将引擎切换为 xelatex (原生支持 Unicode/中文)
 latex_engine = 'xelatex'
-latex_use_xindy = False
+# latex_use_xindy = False
 # 2. 配置 LaTeX 导言区以支持中文渲染
 latex_elements = {
     'papersize': 'a4paper',
